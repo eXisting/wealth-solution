e9e828a816c82f84d8c392dbdcc38e02186765a1
Andrey Popazov <andrey.plagiatix@gmail.com>
2024-04-09 21:08:09 +0400

    Revert "Last react version"
    This reverts commit 662d4a91e6ca18cfaea06099748597056f37aa0a.

--- app/src/index.js
+++ app/src/index.js
@@ -1,47 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
-import WelcomePage from './Routes/WelcomePage';
-import WelcomeSavingsFormulaPage from './Routes/WelcomeSavingsFormulaPage';
-import SavingsGraphPage from './Routes/SavingsGraphPage';
-import WealthRulesPage from './Routes/WealthRulesPage';
-import InitialValuesPage from './Routes/InitialValuesPage';
-import DecadeOnePage from './Routes/DecadeOnePage';
-import DecadeTwoPage from './Routes/DecadeTwoPage';
-import DecadeThreePage from './Routes/DecadeThreePage';
-import PreResultPage from './Routes/PreResultPage';
-import CalculatedPage from './Routes/CalculatedPage';
-import FollowSteps from './Routes/FollowSteps';
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const RootApp = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Router>
-          <RootApp>
-            <Routes>
-              <Route path="/" element={<WelcomePage store={store}/>} />
-              <Route path="/initial-data" element={<InitialValuesPage />} />
-              <Route path="/savings-graph" element={<SavingsGraphPage />} />
-              <Route path="/decade-one" element={<DecadeOnePage />} />
-              <Route path="/decade-two" element={<DecadeTwoPage />} />
-              <Route path="/decade-three" element={<DecadeThreePage />} />
-              <Route path="/pre-result" element={<PreResultPage />} />
-              <Route path="/calculated" element={<CalculatedPage />} />
-              <Route path="/follow-steps" element={<FollowSteps />} />
-            </Routes>
-          </RootApp>
-        </Router>
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
