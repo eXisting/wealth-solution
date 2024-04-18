33d261cceb4ac1a2684b2d8f8b5745bf84255fea
Andrey Popazov <andrey.plagiatix@gmail.com>
2024-04-18 21:11:43 +0400

    Remove horizontal scrolling

--- app/src/index.js
+++ app/src/index.js
@@ -10,8 +10,8 @@ import UseWealthometer from './FigmaUX/UseWealthometer';
 import CalculateFromIncome from './FigmaUX/CalculateFromIncome';
 
 const RootApp = styled.div`
-  width: 100vw;
-  height: 100vh;
+  margin: 0;
+  padding: 0;
 `;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
